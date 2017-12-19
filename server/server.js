08f1ee9d423e123493df1468206c72b97e58571a
Tomasz89 <TomaszRybacki1989@gmail.com>
2017-12-19 20:01:39 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -9,18 +10,40 @@ let app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (request, response) => {
-  let todo = new Todo({
-    text: request.body.text
-  });
+//app.post('/todos', (request, response) => {
+//  let todo = new Todo({
+//    text: request.body.text
+//  });
+//
+//  todo.save().then((document) => {
+//    response.send(document);
+//  }, (error) => {
+//    response.status(400).send(error);
+//  });
+//});
+//
+//app.get('/todos', (request, response) => {
+//  Todo.find().then((todos) => {
+//    response.send({todos});
+//  }, (error) => {
+//    response.status(400).send(error);
+//  });
+//});
 
-  todo.save().then((document) => {
-    response.send(document);
-  }, (error) => {
-    response.status(400).send(error);
-  });
+app.get('/todos/:id', (request, response) => {
+  let id = request.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return response.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+  if (!todo) {
+    return response.status(404).send();
+  }
+  response.send({todo});
+  }).catch((e) => response.status(400).send());
 });
-
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-// run before each test case and remove all todos from database
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
+// run before each test case
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -27,7 +35,7 @@ describe('POST /todos', () => {
       }
 
       // cheack the real database item
-      Todo.find().then((todos) => {
+      Todo.find({text}).then((todos) => {
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -46,10 +54,24 @@ describe('POST /todos', () => {
       }
 
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((e) => done(e));
     });
   });
 
 });
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((response) => {
+      expect(response.body.todos.length).toBe(2)
+    })
+    .end(done);
+  });
+
+});
+
